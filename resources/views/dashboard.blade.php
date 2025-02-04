b9536d6fc740806411a26903598fdb51d4d3257a
Ahmad Fahrezi <ahmadfahrezir@gmail.com>
2025-02-04 20:03:35 +0700

    mempercantik dashboard user

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -160,7 +160,6 @@
                             <div class="absolute inset-0 bg-black bg-opacity-50 opacity-0 group-hover:opacity-100 
                                       transition duration-300 flex items-center justify-center">
                                 <div class="text-center p-4">
-                                    <h3 class="text-xl font-bold text-white mb-2">{{ $place['name'] }}</h3>
                                     <p class="text-gray-200">{{ $place['description'] }}</p>
                                     <a href="#" class="inline-block mt-4 px-6 py-2 bg-red-600 text-white rounded-full 
                                                   hover:bg-red-700 transition duration-300">Read More</a>
--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -15,14 +15,20 @@
         @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
     <body class="font-sans text-gray-900 antialiased">
-        <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100 dark:bg-gray-900">
-            <div>
+        <div style="background-image: url('{{asset('img/kota-semarang.png')}}');" 
+             class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-cover bg-center">
+            <!-- Overlay gelap untuk membuat teks lebih mudah dibaca -->
+            <div class="absolute inset-0 bg-black/50"></div>
+            
+            <!-- Logo -->
+            <div class="relative z-10">
                 <a href="/">
-                    <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
+                    <x-application-logo class="w-20 h-20 fill-current text-white" />
                 </a>
             </div>
 
-            <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white dark:bg-gray-800 shadow-md overflow-hidden sm:rounded-lg">
+            <!-- Form Container -->
+            <div class="relative z-10 w-full sm:max-w-md mt-6 px-6 py-4 bg-white/80 dark:bg-gray-800/80 shadow-md overflow-hidden sm:rounded-lg backdrop-blur-sm">
                 {{ $slot }}
             </div>
         </div>
