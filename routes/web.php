481311b3bc938a5c19ae60f8ee3d117efddea3a5
Ahmad Fahrezi <ahmadfahrezir@gmail.com>
2025-02-05 20:33:22 +0700

    modifikasi dashboard ketika dikunjungi user

--- routes/web.php
+++ routes/web.php
@@ -11,31 +11,31 @@ use App\Http\Controllers\ShoppingController;
 use App\Http\Controllers\EntertainmentController;
 use App\Http\Controllers\TourGuideController;
 
+// routes/web.php
 Route::get('/', [App\Http\Controllers\DashboardController::class, 'index'])
-    ->middleware(['auth'])
     ->name('dashboard');
 
-    Route::middleware(['auth'])->group(function () {
-        Route::get('/hotel', [HotelController::class, 'index'])->name('hotel.index');
-        Route::get('/hotel/{id}', [HotelController::class, 'show'])->name('hotel.show');
-        Route::get('/culinary', [CulinaryController::class, 'index'])->name('culinary.index');
-        Route::get('/culinary/{id}', [CulinaryController::class, 'show'])->name('culinary.show');
-        Route::get('/shopping', [ShoppingController::class, 'index'])->name('shopping.index');
-        Route::get('/shopping/{id}', [ShoppingController::class, 'show'])->name('shopping.show');
-        Route::get('/entertainment', [EntertainmentController::class, 'index'])->name('entertainment.index');
-        Route::get('/entertainment/{id}', [EntertainmentController::class, 'show'])->name('entertainment.show');
-        Route::get('/tourist-attraction', [TouristAttractionController::class, 'index'])->name('tourist-attraction.index');
-        Route::get('/tourist-attraction/{id}', [TouristAttractionController::class, 'show'])->name('tourist-attraction.show');
-        Route::get('/tour-guide', [TourGuideController::class, 'index'])->name('tour-guide.index');
-        Route::get('/tour-guide/{id}', [TourGuideController::class, 'show'])->name('tour-guide.show');
-    });
+// Public routes
+Route::get('/hotel', [HotelController::class, 'index'])->name('hotel.index');
+Route::get('/hotel/{id}', [HotelController::class, 'show'])->name('hotel.show');
+Route::get('/culinary', [CulinaryController::class, 'index'])->name('culinary.index');
+Route::get('/culinary/{id}', [CulinaryController::class, 'show'])->name('culinary.show');
+Route::get('/shopping', [ShoppingController::class, 'index'])->name('shopping.index');
+Route::get('/shopping/{id}', [ShoppingController::class, 'show'])->name('shopping.show');
+Route::get('/entertainment', [EntertainmentController::class, 'index'])->name('entertainment.index');
+Route::get('/entertainment/{id}', [EntertainmentController::class, 'show'])->name('entertainment.show');
+Route::get('/tourist-attraction', [TouristAttractionController::class, 'index'])->name('tourist-attraction.index');
+Route::get('/tourist-attraction/{id}', [TouristAttractionController::class, 'show'])->name('tourist-attraction.show');
+Route::get('/tour-guide', [TourGuideController::class, 'index'])->name('tour-guide.index');
+Route::get('/tour-guide/{id}', [TourGuideController::class, 'show'])->name('tour-guide.show');
 
-    Route::middleware('auth')->group(function () {
-        Route::get('/profile', [ProfileController::class, 'index'])->name('profile.userProfile');
-        Route::get('/profile/edit', [ProfileController::class, 'edit'])->name('profile.edit');
-        Route::patch('/profile/edit', [ProfileController::class, 'update'])->name('profile.update');
-        Route::delete('/profile/edit', [ProfileController::class, 'destroy'])->name('profile.destroy');
-    });
+// Protected routes
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'index'])->name('profile.userProfile');
+    Route::get('/profile/edit', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile/edit', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile/edit', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
     
 
 require __DIR__.'/auth.php';
