840c1345b7c6bc8edade553dee93e25782b1d8b7
reeru14 <fakhrella@gmail.com>
2025-02-13 01:10:03 +0700

    update apa aja

--- app/Http/Controllers/AddCulinaryController.php
+++ app/Http/Controllers/AddCulinaryController.php
@@ -3,7 +3,9 @@
 namespace App\Http\Controllers;
 
 use App\Models\Culinary;
+use App\Models\Menu;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
 
 class AddCulinaryController extends Controller
 {
@@ -12,11 +14,37 @@ class AddCulinaryController extends Controller
         $culinaries = Culinary::all(); // Fetch all culinary data
         return view('admin.addculinary', compact('culinaries')); // Pass it to the view
     }
+
+    public function destroy($id)
+    {    
+        menu::where('culinaryID', $id)->delete();
+        $culinary = Culinary::findOrFail($id);
+        $culinary->delete();
+        return redirect()->route('addculinary.index');
+    }
     
     public function edit($id)
     {
         $culinary = Culinary::findOrFail($id);
-        return view('admin.editculinary', compact('culinary'));
+        return view('admin.addculinaryedit', compact('culinary'));
+    }
+
+    public function update(Request $request, $id)
+    {
+        $culinary = Culinary::findOrFail($id);
+        $culinary->name = $request->culinary_name;
+        $culinary->desc = $request->description;
+
+        if ($request->hasFile('image')) {
+            if ($culinary->image) {
+                Storage::delete('storage/' . $culinary->image);
+            }
+            $imagePath = $request->file('image')->store('img/culinaries', 'public');
+            $culinary->image = $imagePath;
+        }
+
+        $culinary->save();
+        return redirect()->route('addculinary.index');
     }
     
 
